51239c58b125c1b60e3ad1f4a6d458f87161aad0
jessica <jessica.bermeo@optima.net.co>
2023-01-05 16:33:16 -0500

    qwerty

---
+++ ProjectSourceTree/src/app/jessica/jessica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jessica',
+  templateUrl: './jessica.component.html',
+  styleUrls: ['./jessica.component.scss']
+})
+export class JessicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
